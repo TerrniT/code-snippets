35125bdd126d7dc384aef76c88393dd9c5f16b46
TerrniT <terrnit.megaman@gmail.com>
2023-04-16 12:32:13 +0500

    chore(init): scalfolding project with skeleton-app@latest

---
+++ src/components/code-snippet-card.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { CodeBlock } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="card">
+	<header class="card-header">
+		Title of Snippet
+		<div class="float-right">
+			<button class="btn btn-sm variant-filled-secondary" type="button"> Favorite </button>
+			<button class="btn btn-sm variant-filled-error" type="button"> Delete </button>
+		</div>
+	</header>
+	<section class="p-4">
+		<CodeBlock language={'HTML'} code={`<h1>Hello </h1>`} />
+	</section>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import CodeSnippetCard from '../components/code-snippet-card.svelte';
+</script>
+
+<div class="flex justify-center">
+	<div class="grid grid-cols-1 gap-4 mix-w-full md:min-w-[750px]">
+		<h3 class="text-center py-6">Create a Code Snippet</h3>
+		<div class="card p-4 w-full text-token space-y-4">
+			<label class="label">
+				<span>Snippet Title</span>
+				<input class="input" type="text" placeholder="Enter title here ..." />
+			</label>
+
+			<label class="label">
+				<span>Programming Language</span>
+				<select class="select">
+					<option value="HTML">HTML</option>
+					<option value="CSS">HTML</option>
+					<option value="Typescript">HTML</option>
+					<option value="Javascript">HTML</option>
+					<option value="Rust">HTML</option>
+				</select>
+			</label>
+
+			<label class="label">
+				<span>Code Snippet</span>
+				<textarea class="textarea" placeholder="Enter Your Snippet here " rows="4" />
+			</label>
+		</div>
+		<div class="text-center py-6">
+			<h2>Code Snippets</h2>
+		</div>
+		<CodeSnippetCard />
+	</div>
+</div>
